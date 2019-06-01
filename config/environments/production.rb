0f8c957314c46ca9b9ed02b7a287306c46dc1952
KS <lookatachic@gmail.com>
2019-05-31 18:06:23 -0700

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  # SSLによる安全な通信（本番環境のみ）
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
