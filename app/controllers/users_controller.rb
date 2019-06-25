ff3a4a6d65a80a48b15186f28b84f7333c20f1db
KS <lookatachic@gmail.com>
2019-06-25 16:16:01 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  # コントローラ内の特定のアクションにのみbeforeアクションを適用【onlyオプション】
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def show
     #送られてきたIDからUserを特定して、インスタンス変数にセット
@@ -23,9 +27,61 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    # ユーザー情報を編集するアクション
+    # @user = User.find(params[:id]) ←correct_userで定義しているためコメント
+  end
+
+  def update
+    # @user = User.find(params[:id]) ←correct_userで定義しているためコメント
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  # ユーザーのindexアクション
+  def index
+    # @users = User.all
+    @users = User.paginate(page: params[:page])
+  end
+
+  # destroyアクション
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
       def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        # ここで許可されたパラメータのみユーザーは編集可能
+        params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+      end
+
+      # BEFOREアクション
+      # ログイン済みユーザーかどうか確認
+      def logged_in_user
+        unless logged_in?
+          store_location
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
+      end
+
+      # 正しいユーザーかどうか確認
+      def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+      end
+
+      # 管理者かどうか確認
+      def admin_user
+        redirect_to(root_url) unless current_user.admin
       end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -14,3 +14,7 @@ on_worker_boot do
   # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
+
+# デフォルトは60秒でタイムアウト
+# byebugコンソール動作不良改善
+worker_timeout 500
